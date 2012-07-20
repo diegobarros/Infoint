0e2b19e57379d51bf1253a8f2cb24ca25be4b19f
Diego Augusto de Faria Barros <diego@diego-IPMH61R2.(none)>
2012-07-20 11:31:31 -0300

    Criação do Projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Infoint::Application.config.secret_token = 'c59dcac50cfc2e6ec268f81dc113077db55bdc2ec362ac247289dd5a046bab5b70a705e3a4a2b4f533514049cbbc9d535b84005838d4d199f71e7059485eff7e'
