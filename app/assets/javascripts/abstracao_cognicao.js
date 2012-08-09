16d20e04cbd5ad314ca2edc8cfd3c19c2f17dc28
Diego Augusto de Faria Barros <diego@diego-IPMH61R2.(none)>
2012-08-09 16:06:59 -0300

    Implementacao da resposta no formulario

--- app/assets/javascripts/abstracao_cognicao.js
+++ app/assets/javascripts/abstracao_cognicao.js
@@ -4,9 +4,13 @@
 $(document).ready(function(){
 
 	$("#rodape-perguntas ul li:first-child a").click(function(event){
+
 		var idBotaoResponderPergunta =  $(this).attr('id');
 		var idBotaoAvancarPergunta = idBotaoResponderPergunta.substring(idBotaoResponderPergunta.lastIndexOf('-') + 1);
 		
+		var isChecked = $("input[name=alternativa]:checked").length > 0
+		
+	
 		$("#botao-avancar-pergunta-" + idBotaoAvancarPergunta ).css('visibility','visible');
 		
 		$("#container-respostas-" + idBotaoAvancarPergunta ).css('visibility','visible');
